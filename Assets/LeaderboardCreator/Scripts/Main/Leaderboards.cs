60e3d022d7c04d061faa5224d02d700a7054e9eb
Romyhasit <rvngalaxy@gmail.com>
2025-10-30 20:54:42 +0100

    Made a script for the Leaderboard
    Adada

---
+++ Assets/LeaderboardCreator/Scripts/Main/Leaderboards.cs
@@ -0,0 +1,8 @@
+namespace Dan.Main
+{
+    public static class Leaderboards
+    {
+        public static LeaderboardReference DemoSceneLeaderboard = new LeaderboardReference("980324f10e2f03fc0fd6a38a70f88123919a6aed35a4aea86211ad58b68db9c4");
+        public static LeaderboardReference RowdyRiversLeaderboard = new LeaderboardReference("923e62225c92aac66b29f5557b3e18784a408de53c53b05f26a5bc5f3885da82");
+    }
+}
